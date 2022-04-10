dd62aced707143dbd7d2430864d2337f7e53e55f
Prakash <choudharyprakash066@gmail.com>
2022-04-10 20:34:05 +0530

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use mongodb::{bson::doc, options::ClientOptions, Client};
+#[tokio::main]
+
+async fn main() -> mongodb::error::Result<()> {
+    let client_options = ClientOptions::parse(
+        "mongodb+srv://dcsignal:dcsignal%40n1@cluster0.l8joh.mongodb.net/SigBuffer?retryWrites=true&w=majority",
+    ).await.unwrap();
+
+    let client = Client::with_options(client_options).unwrap();
+
+    let buffer = client.database("SigBuffer").collection("buffer");
+
+    // println!("Databases:");
+
+    let new_doc = doc! {
+        "Address": "0x000"
+    };
+    
+    let insert_result = buffer.insert_one(new_doc.clone(), None).await.unwrap();
+
+    println!("New document ID: {}", insert_result.inserted_id);
+    
+   //  for name in client.list_database_names(None, None).await.unwrap() {
+   //    println!("- {}", name);
+   //  }
+    Ok(())
 }
